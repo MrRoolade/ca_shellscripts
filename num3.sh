a85b7657c14d60611852e1384b1860aeef673489
MrRoolade <romain.recasens@free.fr>
2023-04-07 10:57:17 +0200

    ajout du fichier num3.sh exercice 3

---
+++ num3.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+# this script give the number of arguments you give after itself
+    # think you are inside the file
+    # Change here
+    echo $#
+
+# Do not change anything
+if [ ! $# -lt 1 ]; then
+    echo $*
+    exit 0
+fi
